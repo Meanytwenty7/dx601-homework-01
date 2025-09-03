dfb92b60fe7a8e05842e7141c921ff98eaf17be6
Paolo Meany <udn5cq@bu.edu>
2025-09-03 00:56:08 +0000

    Save homework progress

---
+++ homework.ipynb.py
@@ -0,0 +1,401 @@
+# coding: utf-8
+p1 = 3.1
+p1
+p1 = 3.13
+p1
+p1 = 3.1
+p1
+p2
+p2
+p2
+p2
+q1 = 1
+q1
+p1 = 3.1
+p1
+1 + 3
+5 - 2
+3 * 4
+8 / 3
+q2 = 5 * 7
+q2
+p2 = 9/6
+p2
+q3 = (2 + 8) * 3
+q3
+p3 = ...
+p3
+3 ** 5
+p4 = ...
+import math
+q5 = math.pi
+q5
+p5 = ...
+p5
+q6 = [1, 4, 6]
+q6
+p6 = ...
+p6
+q7a = [2, 3, 5, 7, 11]
+q7a
+q7b = [x**2 for x in q7a]
+q7b
+p7 = ...
+p7
+q8a = [1, 2, 3, 4, 5]
+sum(q8a)
+q8b = [0.4, 6.25, 999, 0.245, 23.5, 31.67, 32.67]
+p8 = sum(...)
+p2
+p3 = (2 + 5)/3
+p3
+p4 = 2 ** 8
+p4 = 2 ** 8
+p4 = 2 ** 8
+p4 = 2 ** 8
+3 ** 5
+p4 = 2 ** 8
+q1 = 1
+q1
+p1 = 3.1
+p1
+1 + 3
+5 - 2
+3 * 4
+8 / 3
+q2 = 5 * 7
+q2
+p2 = 9/6
+p2
+q3 = (2 + 8) * 3
+q3
+p3 = (2 + 5)/3
+p3
+3 ** 5
+p4 = 2 ** 8
+import math
+q5 = math.pi
+q5
+p5 = ...
+p5
+q6 = [1, 4, 6]
+q6
+p6 = ...
+p6
+q7a = [2, 3, 5, 7, 11]
+q7a
+q7b = [x**2 for x in q7a]
+q7b
+p7 = ...
+p7
+q8a = [1, 2, 3, 4, 5]
+sum(q8a)
+q8b = [0.4, 6.25, 999, 0.245, 23.5, 31.67, 32.67]
+p8 = sum(...)
+p4 = 2 ** *
+p4 = 2 ** 8
+p4 = 
+p4 
+import math
+q1 = 1
+q1
+p1 = 3.1
+p1
+1 + 3
+5 - 2
+3 * 4
+8 / 3
+q2 = 5 * 7
+q2
+p2 = 9/6
+p2
+q3 = (2 + 8) * 3
+q3
+p3 = (2 + 5)/3
+p3
+3 ** 5
+p4 = 2 ** 8
+p4 
+import math
+p5 = q5 ** 2
+q1 = 1
+q1
+p1 = 3.1
+p1
+1 + 3
+5 - 2
+3 * 4
+8 / 3
+q2 = 5 * 7
+q2
+p2 = 9/6
+p2
+q3 = (2 + 8) * 3
+q3
+p3 = (2 + 5)/3
+p3
+3 ** 5
+p4 = 2 ** 8
+p4 
+import math
+q5 = math.pi
+q5
+p5 = q5 ** 2
+p5
+q6 = [1, 4, 6]
+q6
+p6 = ...
+p6
+q7a = [2, 3, 5, 7, 11]
+q7a
+q7b = [x**2 for x in q7a]
+q7b
+p7 = ...
+p7
+q8a = [1, 2, 3, 4, 5]
+sum(q8a)
+q8b = [0.4, 6.25, 999, 0.245, 23.5, 31.67, 32.67]
+p8 = sum(...)
+p6 = [0,2,3]
+p6
+p7 = [5,10,26,49,122]
+p7
+p7 = q7b + 1
+p7 = [5,10,26,49,122]
+p7
+p7 = [x + 1 for x in q7b]
+p7
+q7a = [2,3,5,7,11]
+q7a
+p8 = sum(...)
+p8 = sum q8b
+q8b = [0.4, 6.25, 999, 0.245, 23.5, 31.67, 32.67]
+q8b = [0.4, 6.25, 999, 0.245, 23.5, 31.67, 32.67]
+p8 = sum of q8b
+p8 = sum (q8b)
+p8 = sum(q8b)
+p8
+len(q9a)
+len(q9a)
+q9a = [8.6, 1.5, 8.6, 0.3, 8.2, 2.5, 6.5, 0.0, 8.4, 7.8]
+len(q9a)
+q9b = [7.0, 3.9, 7.8, 3.7, 5.8, 6.7, 4.8, 3.8]
+p9 = len(q9b)
+p9
+q10 = [8.0, 0.9, 8.4, 3.5, 9.4, 9.4, 1.8, 2.9, 9.2, 5.1]
+p10 = sum(q10)
+p10
+q11 = [5.2, 9.6, 8.0, 2.4, 0.4, 1.8, 5.5]
+p11 = len(q11)
+p11
+q12a = [3, 4, 5, 6, 7]
+q12b = sum([x * x for x in q12a])
+q12b
+q12c = [2, 3, 4, 5]
+p12 = sum( [x ** 3 for x in q12c])
+p12
+# load pandas module and make it accessible as "pd"
+import pandas as pd
+# read the f13a.tsv as a tab-separated file.
+# the sep parameter tells pandas how to parse the file,
+# and will be used for most files in this course.
+q13 = pd.read_csv("f13a.tsv", sep="\t")
+# print out a preview of the data in q11.
+# note the column names in the preview.
+q13
+# load pandas module and make it accessible as "pd"
+import pandas as pd
+# read the f13a.tsv as a tab-separated file.
+# the sep parameter tells pandas how to parse the file,
+# and will be used for most files in this course.
+q13 = pd.read_csv("f13a.tsv", sep="\t")
+# print out a preview of the data in q11.
+# note the column names in the preview.
+q13
+# read f13b.tsv and save it as p13, following the example for q13.
+p13 = ...
+# read f13b.tsv and save it as p13, following the example for q13.
+p13 = ...
+# read f13b.tsv and save it as p13, following the example for q13.
+p13 = pd.read_csv("f13b.tsv", sep="\t")
+p13 =
+p13 =
+# read f13b.tsv and save it as p13, following the example for q13.
+p13 = pd.read_csv("f13b.tsv", sep="\t")
+p13 =
+p13 = pd.read_csv("f13a.tsv", sep="\t")
+# read f13b.tsv and save it as p13, following the example for q13.
+p13 = pd.read_csv("f13b.tsv", sep="\t")
+p13 = pd.read_csv("f13a.tsv", sep="\t")
+p13 =
+p13 =
+p13 =
+# read f13b.tsv and save it as p13, following the example for q13.
+p13 = pd.read_csv("f13b.tsv", sep="\t")
+p13 =
+p13 
+q14 = pd.read_csv("https://raw.githubusercontent.com/bu-cds-omds/dx601-examples/main/data/abalone.tsv", sep="\t")
+q14
+q14.columns
+# column names are text data, so use quotes to save them in a variable.
+p14 = "..."
+# column names are text data, so use quotes to save them in a variable.
+p14 = "..."
+p14
+# column names are text data, so use quotes to save them in a variable.
+p14 = "p14"
+p14
+q14
+# column names are text data, so use quotes to save them in a variable.
+p14 = "Sex"
+p14
+# column names are text data, so use quotes to save them in a variable.
+p14 = q14.columns(1)
+p14
+# column names are text data, so use quotes to save them in a variable.
+p14 = q14.columns[1]
+p14
+# column names are text data, so use quotes to save them in a variable.
+p14 = q14.columns[0]
+p14
+import numpy as np
+q15a = [1, 3, 8, 6]
+q15b = np.array(q15a)
+q15b
+q15c = [2, 3, 5, 9]
+p15 = np.array(q15c)
+p15
+q16a = 1e10
+q16a
+q16b = 22 / 7
+q16b
+p16 = sum q16a + q16b
+p16
+p16 = sum q16a + q16b
+p16 = sum [q16a + q16b]
+p16 = q16a + q16b
+p16
+p17 = sum(q17)
+q17 = [9.1, 4.3, 1.7, 2.8, 2.5, 1.5, 1.4, 3.6, 5.8, 2.3]
+p17 = sum(q17)
+p17
+q18 = [7.1, 5.9, 8.0, 8.3, 8.1, 0.3, 6.1, 7.0, 7.0, 4.8]
+p18 = sum(x*x)
+p18 = sum(x * x for x in q18)
+p18
+p19 = sum (x ** x for x in q19)
+q19 = [4.5, 9.8, 5.7, 5.7, 2.4, 4.7, 2.0, 3.3, 3.5, 4.4]
+p19 = sum (x ** x for x in q19)
+p19
+q18 = [7.1, 5.9, 8.0, 8.3, 8.1, 0.3, 6.1, 7.0, 7.0, 4.8]
+p18 = sum(x ** 2 for x in q18)
+p18
+p19 = sum (x ** 3 for x in q19)
+q19 = [4.5, 9.8, 5.7, 5.7, 2.4, 4.7, 2.0, 3.3, 3.5, 4.4]
+p19 = sum (x ** 3 for x in q19)
+p19
+import pandas as pd
+
+p20 = pd.read_csv("f20.tsv", sep="\t")
+p20
+q1 = 1
+q1
+p1 = 3.1
+p1
+1 + 3
+5 - 2
+3 * 4
+8 / 3
+q2 = 5 * 7
+q2
+p2 = 9/6
+p2
+q3 = (2 + 8) * 3
+q3
+p3 = (2 + 5)/3
+p3
+3 ** 5
+p4 = 2 ** 8
+p4 
+import math
+q5 = math.pi
+q5
+p5 = q5 ** 2
+p5
+q6 = [1, 4, 6]
+q6
+p6 = [0,2,3]
+p6
+q7a = [2,3,5,7,11]
+q7a
+q7b = [x**2 for x in q7a]
+q7b
+p7 = [x + 1 for x in q7b]
+p7
+q8a = [1, 2, 3, 4, 5]
+sum(q8a)
+q8b = [0.4, 6.25, 999, 0.245, 23.5, 31.67, 32.67]
+p8 = sum(q8b)
+p8
+q9a = [8.6, 1.5, 8.6, 0.3, 8.2, 2.5, 6.5, 0.0, 8.4, 7.8]
+len(q9a)
+q9b = [7.0, 3.9, 7.8, 3.7, 5.8, 6.7, 4.8, 3.8]
+p9 = len(q9b)
+p9
+q10 = [8.0, 0.9, 8.4, 3.5, 9.4, 9.4, 1.8, 2.9, 9.2, 5.1]
+p10 = sum(q10)
+p10
+q11 = [5.2, 9.6, 8.0, 2.4, 0.4, 1.8, 5.5]
+p11 = len(q11)
+p11
+q12a = [3, 4, 5, 6, 7]
+q12b = sum([x * x for x in q12a])
+q12b
+q12c = [2, 3, 4, 5]
+p12 = sum( [x ** 3 for x in q12c])
+p12
+# load pandas module and make it accessible as "pd"
+import pandas as pd
+# read the f13a.tsv as a tab-separated file.
+# the sep parameter tells pandas how to parse the file,
+# and will be used for most files in this course.
+q13 = pd.read_csv("f13a.tsv", sep="\t")
+# print out a preview of the data in q11.
+# note the column names in the preview.
+q13
+# read f13b.tsv and save it as p13, following the example for q13.
+p13 = pd.read_csv("f13b.tsv", sep="\t")
+p13 
+q14 = pd.read_csv("https://raw.githubusercontent.com/bu-cds-omds/dx601-examples/main/data/abalone.tsv", sep="\t")
+q14
+q14.columns
+# column names are text data, so use quotes to save them in a variable.
+p14 = q14.columns[0]
+p14
+import numpy as np
+q15a = [1, 3, 8, 6]
+q15b = np.array(q15a)
+q15b
+q15c = [2, 3, 5, 9]
+p15 = np.array(q15c)
+p15
+q16a = 1e10
+q16a
+q16b = 22 / 7
+q16b
+p16 = q16a + q16b
+p16
+q17 = [9.1, 4.3, 1.7, 2.8, 2.5, 1.5, 1.4, 3.6, 5.8, 2.3]
+p17 = sum(q17)
+p17
+q18 = [7.1, 5.9, 8.0, 8.3, 8.1, 0.3, 6.1, 7.0, 7.0, 4.8]
+p18 = sum(x ** 2 for x in q18)
+p18
+q19 = [4.5, 9.8, 5.7, 5.7, 2.4, 4.7, 2.0, 3.3, 3.5, 4.4]
+p19 = sum (x ** 3 for x in q19)
+p19
+import pandas as pd
+
+p20 = pd.read_csv("f20.tsv", sep="\t")
+p20
+get_ipython().run_line_magic('save', '')
